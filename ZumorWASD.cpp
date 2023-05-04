f6343c5e0fa6f89d1d4f65b02a25ed409f8737fb
LaysFu <lololol13246@gmail.com>
2023-05-04 22:25:17 +0200

    revert changes and use functions

--- ZumorWASD.cpp
+++ ZumorWASD.cpp
@@ -4,7 +4,7 @@
 Zumo32U4Buzzer buzzer;
 Zumo32U4Motors motors;
 
-const char crabRave[] PROGMEM =
+const char crabRave[] PROGMEM = // load crab rave song into program space
   "! V10 O5 L8 T125 db-gg16dd16aff16dd16aff16ccee16f16e16"
   "db-gg16dd16aff16dd16aff16ccee16f16e16"
   "db-gg16dd16aff16dd16aff16ccee16f16e16"
@@ -16,63 +16,32 @@ void setup()
   buzzer.stopPlaying();
 }
 
-bool fState = false; // forward
-bool bState = false; // backward
-bool lState = false; // left
-bool rState = false; // right
-
 void loop()
 { 
   if (Serial.available() > 0) {
     char input = Serial.read(); // read input character from serial port
     switch (input) {
       case 'w': // forward
-        fState = true;
+        forward();
         break;
       case 's': // backward
-        bState = true;
+        backward();
         break;
       case 'a': // left
-        lState = true;
+        left();
         break;
       case 'd': // right
-        rState = true;
-        break;
-      case 'rw': // forward release
-        fState = false;
-        break;
-      case 'rs': // backward release
-        bState = false;
-        break;
-      case 'ra': // left release
-        lState = false;
-        break;
-      case 'rd': // right release
-        rState = false;
+        right();
         break;
       case 'f': // play music
-            buzzer.playFromProgramSpace(crabRave);
-            break;
+        buzzer.playFromProgramSpace(crabRave);
+        break;
       case 'e': // stop music
-            buzzer.stopPlaying();
-            break;
-    }
-    
-    // call functie afhankelijk van input
-    if (fState && !bState && !lState && !rState) {
-      forward();
-    } else if (bState && !fState && !lState && !rState) {
-      backward();
-    } else if (lState && !rState) {
-      left();
-    } else if (rState && !lState) {
-      right();
-    } else if (lState && bState && !rState && !fState) {
-      bLeft();
-    } else if (rState && bState && !lState && !fState) {
-      bRight();
-    } else { // stop als er geen input is
-      stop();
+        buzzer.stopPlaying();
+        break;
+      default: // stop if invalid input received
+        stop();
+        break;
     }
   }
 }
@@ -86,19 +55,11 @@ void backward() {
 }
 
 void left() {
-  motors.setSpeeds(50, 200)
+  motors.setSpeeds(-100, 100)
 }
 
 void right() {
-  motors.setSpeeds(200, 50);
-}
-
-void bLeft() {
-  motors.setSpeeds(-50, -200);
-}
-
-void bRight() {
-  motors.setSpeeds(-200, -50);
+  motors.setSpeeds(100, -100);
 }
 
 void stop() {
