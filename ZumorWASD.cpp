d730424925b0ad48c682e08f1e2ca9542a8b95b0
LaysFu <lololol13246@gmail.com>
2023-05-12 17:37:35 +0200

    use classes

--- ZumorWASD.cpp
+++ ZumorWASD.cpp
@@ -1,79 +1,112 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 
-Zumo32U4Buzzer buzzer;
-Zumo32U4Motors motors;
+class CrabRaveSong {
+public:
+  CrabRaveSong() {}
+  static const char song[] PROGMEM;
+};
 
-const char crabRave[] PROGMEM = // load crab rave song into program space
-  "! V10 O5 L8 T125 db-gg16dd16aff16dd16aff16ccee16f16e16"
+const char CrabRaveSong::song[] PROGMEM =
+  "! V10 O5 L8 T125" 
+  "db-gg16dd16aff16dd16aff16ccee16f16e16"
   "db-gg16dd16aff16dd16aff16ccee16f16e16"
   "db-gg16dd16aff16dd16aff16ccee16f16e16"
   "db-gg16dd16aff16dd16aff16ccee16f16e16";
 
-void setup()
-{
-  Serial.begin(9600); // serial connection
-  buzzer.stopPlaying();
-}
+class ZumoMotors {
+private:
+  Zumo32U4Buzzer buzzer;
+  Zumo32U4Motors motors;
 
-void loop(){ 
-  if (Serial.available() > 0) {
-    char input = Serial.read(); // read input character from serial port
-    switch (input) {
-      case 'w': // forward
-        forward();
-        break;
-      case 's': // backward
-        backward();
-        break;
-      case '1': // forward left
-        left();
-        break;
-      case '2': // forward right
-        right();
-        break;
-      case '3': // backward right
-        bRight();
-        break;
-      case '4': // backward left
-        bLeft();
-        break;
-      case 'f': // play music
-            buzzer.playFromProgramSpace(crabRave);
-            break;
-      case 'e': // stop music
-            buzzer.stopPlaying();
-            break;
-      default: 
-            stop();
+public:
+  ZumoMotors() {}
+  void forward() {
+    motors.setSpeeds(200, 200);
+  }
+
+  void backward() {
+    motors.setSpeeds(-200, -200);
+  }
+
+  void left() {
+    motors.setSpeeds(50, 300);
+  }
+
+  void right() {
+    motors.setSpeeds(300, 50);
+  }
+
+  void bLeft() {
+    motors.setSpeeds(-50, -300);
+  }
+
+  void bRight() {
+    motors.setSpeeds(-300, -50);
+  }
+
+  void stop() {
+    motors.setSpeeds(0, 0);
+  }
+
+  void playSong() {
+    buzzer.playFromProgramSpace(CrabRaveSong::song);
+  }
+
+  void stopSong() {
+    buzzer.stopPlaying();
+  }
+};
+
+class Controller {
+private:
+  ZumoMotors control;
+
+public:
+  Controller() {}
+
+  void controller() {
+    if (Serial.available() > 0) {
+      char input = Serial.read();
+      switch (input) {
+        case 'w':
+          control.forward();
+          break;
+        case 's':
+          control.backward();
+          break;
+        case '1': // forward left
+          control.left();
+          break;
+        case '2': // forward rights
+          control.right();
+          break;
+        case '3': // backward right
+          control.bRight();
+          break;
+        case '4': // backward left
+          control.bLeft();
+          break;
+        case 'f':
+          control.playSong();
+          break;
+        case 'e':
+          control.stopSong();
+          break;
+        default:
+          control.stop();
+      }
     }
   }
+};
+
+Controller controller;
+ZumoMotors motors;
+
+void setup() {
+  Serial.begin(9600);
 }
 
-void forward() {
-  motors.setSpeeds(200, 200);
-}
-
-void backward() {
-  motors.setSpeeds(-200, -200);
-}
-
-void left() {
-  motors.setSpeeds(50, 300);
-}
-
-void right() {
-  motors.setSpeeds(300, 50);
-}
-
-void bLeft() {
-  motors.setSpeeds(-50, -300);
-}
-
-void bRight() {
-  motors.setSpeeds(-300, -50);
-}
-
-void stop() {
-  motors.setSpeeds(0, 0);
+void loop() {
+  controller.controller();
 }
