0dea55e9b8806f076c3330a79d9141318dbeb4df
Lybertas <gdjonge98@outlook.com>
2023-06-05 14:15:53 +0200

    Fixed

---
+++ EindProduct/main/colorVariables.h
@@ -0,0 +1,31 @@
+#ifndef COLORVARIABLES_H
+#define COLORVARIABLES_H
+const int sensorMinValue = 0;
+const int sensorMaxValue = 1000;
+
+const int middleWhiteMin = 0;
+const int middleWhiteMax = 50;
+const int middleBlackMin = 251;
+const int middleBlackMax = 1000;
+const int middleGreenMin = 51;
+const int middleGreenMax = 250;
+
+const int leftWhiteMin = 0;
+const int leftWhiteMax = 200;
+const int leftBrownMin = 520;
+const int leftBrownMax = 700;
+const int leftGrayMin = 210;
+const int leftGrayMax = 515;
+const int leftBlackMin = 705;
+const int leftBlackMax = 2000;
+
+const int rightWhiteMin = 0;
+const int rightWhiteMax = 200;
+const int rightBrownMin = 420;
+const int rightBrownMax = 700;
+const int rightGrayMin = 210;
+const int rightGrayMax = 420;
+const int rightBlackMin = 700;
+const int rightBlackMax = 2000;
+
+#endif //COLORVARIABLES_H
